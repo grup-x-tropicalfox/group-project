a048ce178f134a74cdb82aeeb6e12d1a37a153e5
ziterz <ziady.mubaraq@gmail.com>
2020-09-03 14:00:52 +0700

    latest development

--- server/app.js
+++ server/app.js
@@ -13,11 +13,6 @@ app.post('/login', (req, res) => {
   const { email, password } = req.body;
 
   res.status(200).json({
-    access_token
-  });
-});
-
-  res.status(200).json({
     email,
     password
   });
