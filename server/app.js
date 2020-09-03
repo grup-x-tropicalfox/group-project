b0baf8beec183c63a03881e8c71f902a411ad51a
ziterz <ziady.mubaraq@gmail.com>
2020-09-03 13:55:53 +0700

    fetch data done

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { toUnicode } = require('punycode');
 const app = express();
 const port = 3000;
 app.post('/register', (req, res) => {
@@ -17,6 +18,14 @@ app.post('/login', (req, res) => {
     password
   });
 });
+
+app.get('/data', (req, res) => {
+  toUnicode.findAll()
+   .then(response =>{
+     console.log(response);
+   })
+})
+
 app.listen(port, () => {
   console.log('app listening on port ', port);
 })
